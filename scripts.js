83597dc997df7e979d2836fff9f1de6664ed8baf
Asad Raiyan <asadraiyan001@gmail.com>
2023-12-18 17:45:09 +0530

    refactor the code

--- scripts.js
+++ scripts.js
@@ -5,60 +5,52 @@ const firstBoxStyle = document.getElementById("first-box");
 const middleBoxStyle = document.getElementById("middle-box");
 const lastBoxStyle = document.getElementById("last-box");
 
+// function to change the display and css of the unselected box
+
+function unSelectBox(box, boxSelector) {
+  box.style.display = "none";
+  boxSelector.style.cssText = `
+  border-radius: 3.615px;
+  border: 0.904px solid #c8c8c8;
+  background: #fff;`;
+}
+
+// function to change the display and css of the selected box
+
+function selectBox(box, boxSelector) {
+  box.style.display = "flex";
+  boxSelector.style.cssText = `
+  border-radius: 3.615px;
+  border: 0.904px solid #007F61;
+  background: #F4FBF9;`;
+}
+
+// function to handle the first box
+
 function handleFirstBox(radioButtonId) {
-  console.log("clicked");
-  first_box.style.display = "flex";
-  firstBoxStyle.style.cssText = `
-border-radius: 3.615px;
-border: 0.904px solid #007F61;
-background: #F4FBF9;`;
-  middleBoxStyle.style.cssText = `
- border-radius: 3.615px;
-  border: 0.904px solid #c8c8c8;
-  background: #fff;`;
-  lastBoxStyle.style.cssText = `
-   border-radius: 3.615px;
-  border: 0.904px solid #c8c8c8;
-  background: #fff;`;
+  //   console.log("clicked");
+  selectBox(first_box, firstBoxStyle);
   document.getElementById(radioButtonId).click();
-  middle_box.style.display = "none";
-  last_box.style.display = "none";
+  unSelectBox(middle_box, middleBoxStyle);
+  unSelectBox(last_box, lastBoxStyle);
 }
+
+// function to handle the middle box
+
 function handleMiddleBox(radioButtonId) {
-  console.log("clicked");
+  //   console.log("clicked");
+  selectBox(middle_box, middleBoxStyle);
   document.getElementById(radioButtonId).click();
-  middleBoxStyle.style.cssText = `
-border-radius: 3.615px;
-border: 0.904px solid #007F61;
-background: #F4FBF9;`;
-  firstBoxStyle.style.cssText = `
- border-radius: 3.615px;
-  border: 0.904px solid #c8c8c8;
-  background: #fff;`;
-  lastBoxStyle.style.cssText = `
-   border-radius: 3.615px;
-  border: 0.904px solid #c8c8c8;
-  background: #fff;`;
-  first_box.style.display = "none";
-  last_box.style.display = "none";
-  middle_box.style.display = "flex";
+  unSelectBox(first_box, firstBoxStyle);
+  unSelectBox(last_box, lastBoxStyle);
 }
+
+// function to handle the last box
+
 function handleLastBox(radioButtonId) {
-  console.log("clicked");
-  last_box.style.display = "flex";
-  lastBoxStyle.style.cssText = `
-border-radius: 3.615px;
-border: 0.904px solid #007F61;
-background: #F4FBF9;`;
-  middleBoxStyle.style.cssText = `
- border-radius: 3.615px;
-  border: 0.904px solid #c8c8c8;
-  background: #fff;`;
-  firstBoxStyle.style.cssText = `
-   border-radius: 3.615px;
-  border: 0.904px solid #c8c8c8;
-  background: #fff;`;
+  //   console.log("clicked");
+  selectBox(last_box, lastBoxStyle);
   document.getElementById(radioButtonId).click();
-  first_box.style.display = "none";
-  middle_box.style.display = "none";
+  unSelectBox(first_box, firstBoxStyle);
+  unSelectBox(middle_box, middleBoxStyle);
 }
